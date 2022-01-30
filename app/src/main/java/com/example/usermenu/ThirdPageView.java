fd3826e94b332e4a3ce0cc6c1e2716213ffa9029
akmaaqishah <akmaaqishah@gmail.com>
2022-01-30 18:07:24 +0800

    Update parcel display view

--- app/src/main/java/com/example/usermenu/ThirdPageView.java
+++ app/src/main/java/com/example/usermenu/ThirdPageView.java
@@ -37,8 +37,58 @@ public class ThirdPageView extends AppCompatActivity {
         }
 
 
+        // Create a instance of the database and get
+        // its reference
+        dbase = FirebaseDatabase.getInstance("https://fir-f9b19-default-rtdb.firebaseio.com/").getReference("Parcel");
 
+        dbase.orderByChild("cusId").equalTo(cusId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists())
+                { }
+                else {
+                    Toast.makeText(ThirdPageView.this, "No parcel available ", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        //Create view
+        recyclerView = findViewById(R.id.recycler1);
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(
+                new LinearLayoutManager(this));
+
+
+        // It is a class provide by the FirebaseUI to make a
+        // query in the database to fetch appropriate data
+        FirebaseRecyclerOptions<Parcel> options
+                = new FirebaseRecyclerOptions.Builder<Parcel>()
+                .setQuery(dbase.orderByChild("cusId").equalTo(cusId), Parcel.class)
+                .build();
+        // Connecting object of required Adapter class to
+        // the Adapter class itself
+        adapter = new ParcelAdapter(options);
+        recyclerView.setAdapter(adapter);
 
     }
+    // Function to tell the app to start getting
+    // data from database on starting of the activity
+    @Override protected void onStart()
+    {
+        super.onStart();
+        adapter.startListening();
+    }
 
+    // Function to tell the app to stop getting
+    // data from database on stopping of the activity
+    @Override protected void onStop()
+    {
+        super.onStop();
+        adapter.stopListening();
+    }
 }
