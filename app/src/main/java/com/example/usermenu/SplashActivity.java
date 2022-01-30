3691e86d5caa7a0de1db96ebb68393711d9df603
aimanrozali <aimanrozali@student.usm.my>
2022-01-30 18:12:41 +0800

    Add splash screen for User module

---
+++ app/src/main/java/com/example/usermenu/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.usermenu;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAnimation;
+    ImageView homescreenbg;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Hide status bar
+        WindowInsetsControllerCompat controller = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        controller.hide( WindowInsetsCompat.Type.systemBars());
+
+        //Hide aplication aplication bar
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.hide();
+
+        lottieAnimation = findViewById(R.id.parcelicon);
+        homescreenbg = findViewById(R.id.homescreenbg);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(SplashActivity.this,Login.class);
+                startActivity(in);
+                finish();
+            }
+        },3000);
+    }
+}
